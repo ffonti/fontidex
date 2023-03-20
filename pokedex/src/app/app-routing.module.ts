757c85401a527271ae780d6f836bafc808e10968
Fabrizio <fabriziofontana02@gmail.com>
2023-03-20 15:56:22 +0100

    Config. Tailwind + Angular Routes

--- pokedex/src/app/app-routing.module.ts
+++ pokedex/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CercaPokemonComponent } from './components/cerca-pokemon/cerca-pokemon.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cercaPokemon', component: CercaPokemonComponent },
+  { path: '', component: HomepageComponent, pathMatch: 'full' }, //Default route
+	{ path: '**', component: PageNotFoundComponent } //Errore, pagina non trovata
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CercaPokemonComponent } from './components/cerca-pokemon/cerca-pokemon.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    CercaPokemonComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
